30317dc55658566844125e527f1b65ae31663ca0
Ricardo Miguel <ubinetfone@gmail.com>
2023-09-30 11:45:10 +0200

    delet base vite config

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base:'https://staratlassa.github.io/ManageSA'
+  //base:'https://arawipacha.github.io/'
 })
