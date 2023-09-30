8362eb5b861e51634bedaaac85737f76a18ae1e3
Ricardo Miguel <ubinetfone@gmail.com>
2023-09-30 11:50:44 +0200

    udate base

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base:'StarAtlasSA/ManageSA'
+  base:'https://github.com/arawipacha/StarAtlasSA/ManageSA/'
 })
